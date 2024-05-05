ebfe585f70105aea483377180812a768f7aac092
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-06 00:04:24 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	let position = 0-1;
-  s1 = s1.toLowerCase();
+	s1 = s1.toLowerCase();
   s2 = s2.toLowerCase();
 	for(let i = 0; i < s1.length; i++)
 	{
-	    position += 1;
 	    if(s1[i] == s2[0])
 	    {
 	        if(s1.slice(i, (i+s2.length)) == s2) {
-	            return position;
+	            return i;
 	        }
 	    }
 	}
+	return -1;
 }
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
